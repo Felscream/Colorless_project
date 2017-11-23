accf47e995e45dbcbefde0b4137c83a9fc2be89e
Victor <vicmot@hotmail.fr>
2017-11-23 17:18:20 -0500

    skill progress

--- Assets/Scripts/HUD/SkillButton.cs
+++ Assets/Scripts/HUD/SkillButton.cs
@@ -10,7 +10,10 @@ public class SkillButton : MonoBehaviour
 	private const float SELECTED_COLOR = 1.5f;
 	private const float UNLOCK_COLOR = 2f;
 	private RectTransform recT;
+	[SerializeField]
+	private List<SkillLink> links;
 	private bool unlocked = false;
+	private bool unlockable = false;
 	private bool selected = false;
 	public string descriptionText;
 	public string title;
@@ -61,20 +64,43 @@ public class SkillButton : MonoBehaviour
 
 	public void Unlock()
 	{
-		Color buttonColor = GetComponent<Image>().color;
-		buttonColor.r *= SELECTED_COLOR;
-		buttonColor.g *= SELECTED_COLOR;
-		buttonColor.b *= SELECTED_COLOR;
-		GetComponent<Image>().color = buttonColor;
+		if (unlockable && !unlocked)
+		{
+			Color buttonColor = GetComponent<Image>().color;
+			buttonColor.r *= SELECTED_COLOR;
+			buttonColor.g *= SELECTED_COLOR;
+			buttonColor.b *= SELECTED_COLOR;
+			GetComponent<Image>().color = buttonColor;
+			foreach (SkillLink link in links)
+			{
+				link.MakeAvailable();
+			}
+		}
 	}
 
-	public void Lock()
+
+	//Cette fonction ne sert pas. Vous pouvez la supprimer
+	/*public void Lock()
 	{
 		Color buttonColor = GetComponent<Image>().color;
 		buttonColor.r /= SELECTED_COLOR;
 		buttonColor.g /= SELECTED_COLOR;
 		buttonColor.b /= SELECTED_COLOR;
 		GetComponent<Image>().color = buttonColor;
+	}*/
+
+
+	public bool IsUnlocked()
+	{
+		return unlocked;
+	}
+	public bool IsUnlockable()
+	{
+		return unlockable;
+	}
+	public void MakeAvailable()
+	{
+		unlockable = true;
 	}
 
 }
---
+++ Assets/SkillLink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillLink : MonoBehaviour {
+
+	[SerializeField]
+	private SkillButton origin, destination;
+
+
+
+
+	public void MakeAvailable()
+	{
+		destination.MakeAvailable();
+		GetComponent<Image>().color = Color.green;
+
+	}
+
+}
