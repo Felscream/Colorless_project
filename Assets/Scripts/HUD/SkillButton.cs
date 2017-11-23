a4aedfba29149c7c10be89c9cf823893f1dfd9c4
Victor <vicmot@hotmail.fr>
2017-11-23 16:09:41 -0500

    pannel

--- Assets/Scripts/HUD/SkillButton.cs
+++ Assets/Scripts/HUD/SkillButton.cs
@@ -1,27 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class SkillButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class SkillButton : MonoBehaviour
 {
-	private float RESIZE_SCALE = 1.1f;
+	private const float SELECTED_SCALE = 100f;
+	private const float BASE_SCALE = 70f;
+	private const float SELECTED_COLOR = 1.5f;
+	private const float UNLOCK_COLOR = 2f;
 	private RectTransform recT;
+	private bool unlocked = false;
+	private bool selected = false;
+	public string descriptionText;
+	public string title;
+	public string type;
+	public int cost;
 
-	void Start()
+
+	void Awake()
 	{
 		recT = GetComponent<RectTransform>();
 	}
 
-	//Do this when the cursor enters the rect area of this selectable UI object.
-	public void OnPointerEnter(PointerEventData eventData)
+	//temp
+	void Update()
 	{
-		recT.sizeDelta = new Vector2(RESIZE_SCALE, RESIZE_SCALE);
 	}
 
-	public void OnPointerExit(PointerEventData eventData)
+
+	//Do this when the cursor enters the rect area of this selectable UI object.
+	public void SelectSkill()
 	{
-		recT.sizeDelta = new Vector2(RESIZE_SCALE, RESIZE_SCALE);
+		if (!selected)
+		{
+			recT.sizeDelta = new Vector2(SELECTED_SCALE, SELECTED_SCALE);
+			Color buttonColor = GetComponent<Image>().color;
+			buttonColor.r *= SELECTED_COLOR;
+			buttonColor.g *= SELECTED_COLOR;
+			buttonColor.b *= SELECTED_COLOR;
+			GetComponent<Image>().color = buttonColor;
+			selected = true;
+		}
+	}
+
+	public void DeselectSkill()
+	{
+		if (selected)
+		{
+			recT.sizeDelta = new Vector2(BASE_SCALE, BASE_SCALE);
+			Color buttonColor = GetComponent<Image>().color;
+			buttonColor.r /= SELECTED_COLOR;
+			buttonColor.g /= SELECTED_COLOR;
+			buttonColor.b /= SELECTED_COLOR;
+			GetComponent<Image>().color = buttonColor;
+
+			selected = false;
+		}
+	}
+
+	public void Unlock()
+	{
+		Color buttonColor = GetComponent<Image>().color;
+		buttonColor.r *= SELECTED_COLOR;
+		buttonColor.g *= SELECTED_COLOR;
+		buttonColor.b *= SELECTED_COLOR;
+		GetComponent<Image>().color = buttonColor;
+	}
+
+	public void Lock()
+	{
+		Color buttonColor = GetComponent<Image>().color;
+		buttonColor.r /= SELECTED_COLOR;
+		buttonColor.g /= SELECTED_COLOR;
+		buttonColor.b /= SELECTED_COLOR;
+		GetComponent<Image>().color = buttonColor;
 	}
 
 }
